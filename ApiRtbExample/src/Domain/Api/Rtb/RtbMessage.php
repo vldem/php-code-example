728107b377b9236d17fa8ab21080d79f3bdd7984
vldem <103042445+vldem@users.noreply.github.com>
2022-04-11 23:26:25 +0300

    Update RtbMessage.php

--- ApiRtbExample/src/Domain/Api/Rtb/RtbMessage.php
+++ ApiRtbExample/src/Domain/Api/Rtb/RtbMessage.php
@@ -12,7 +12,7 @@ final class RtbMessage
     private Helpper $helpper;
 
     private $messages = [
-        'apiWrongStatus' => "no bid. Respond status is not correct. See the respond header for detailed information:<br>\n",
+        'apiWrongStatus' => "no bid. Respond status is not correct. See the respond header for detailed information.",
         'apiNoBid' => "no bid",
         'requestEmpty' => "no bid. Request string is empty."
     ];
@@ -29,9 +29,9 @@ final class RtbMessage
 
     /**
      * Get message string
-     * @param string $name The message name
-     * @param array $params (optional) list of parameter's tags to substitude with values
-     * @param array $values (optional) list of parameter's values to substitude in tags
+     * @param string $name The message's index name
+     * @param array $params (optional) list of parameter's tags to substitute with values
+     * @param array $values (optional) list of parameter's values to substitute in tags
      * @return string messager string
      */
     public function getMessage( string $name, $params=array(), $values=array() ): string
